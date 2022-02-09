2aba13029885e97f9195a6d3ce12fa4a3842407d
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2022-02-09 23:11:40 +0100

    fix repo is already dl

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Open variables
 source $1
+
 REPOS=$(curl -H "Authorization: token $TOKEN" https://api.github.com/search/repositories\?q\=user:$GITHUB_USERNAME | jq -r ".items[].name")
 echo "Repos: $REPOS"
 
@@ -10,6 +11,14 @@ cd $DESTINATION
 printf "Downloading repos...\n"
 
 for REPO in $REPOS; do
+  # If the repo is already in the destination, just fetch the latest branchs
+  if [ -d $REPO ]; then
+    printf "Repository $REPO is already downloaded\n"
+    cd $(echo $REPO | cut -d "/" -f 2)
+    git fetch --all
+    cd ..
+    continue
+  fi
   git clone https://github.com/$GITHUB_USERNAME/$REPO.git
   cd $(echo $REPO | cut -d "/" -f 2)
   git fetch --all
